8941d8b250ad3ba1036f99a30590e531925adfa1
Lucas <lucas.kominiarz@laposte.net>
2024-10-07 18:05:39 +0200

    heure

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -8,9 +8,13 @@ namespace ConsoleApplication1
         {
             Tab tableau = new Tab();
             
-            Console.Write("Ecrivez un nombre : ");
+            Console.Write("Quel nombre voulez vous convertir : ");
             int value = int.Parse(Console.ReadLine());
-            tableau.CreerTab(value);
+            Console.Write("Voulez-vous convertir en jours (1), en heures (2) ou en minutes (3): ");
+            int value2 = int.Parse(Console.ReadLine());
+            Console.Write("Quel est la grandeur de base(en jour (1), en heures (2) ou en minutes (3)) : ");
+            int value3 = int.Parse(Console.ReadLine());
+            tableau.Convertisseur(value, value2, value3);
            
         }
     }
--- ConsoleApplication1/ConsoleApplication1/Tab.cs
+++ ConsoleApplication1/ConsoleApplication1/Tab.cs
@@ -1,18 +1,63 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace ConsoleApplication1
 {
     public class Tab
     {
-        public void CreerTab(int n)
+        public void Convertisseur(float nb, int conv, int typeBase)
         {
-            int[] tableau = new int[n];
-            int cpt = 0;
-            for (int i = tableau.Length; i > 0; i -= 2)
+            float resultat = 0f;
+            
+            if (conv == 1)
             {
-                tableau[cpt] = i;
-                Console.Write(i+",");
-                cpt -= cpt;
+                if (typeBase == 1)
+                {
+                    resultat = nb;
+                }
+                else if (typeBase == 2)
+                {
+                    resultat = nb / 24;
+                }
+                else
+                {
+                    resultat = nb / (24 * 60);
+                }
+                Console.Write(Math.Truncate(resultat*100)/100);
+            }
+            
+            else if (conv == 2)
+            {
+                if (typeBase == 1)
+                {
+                    resultat = nb * 24;
+                }
+                else if (typeBase == 2)
+                {
+                    resultat = nb;
+                }
+                else
+                {
+                    resultat = nb / 60;
+                }
+                Console.Write(Math.Truncate(resultat*100)/100);
+            }
+            
+            else
+            {
+                if (typeBase == 1)
+                {
+                    resultat = nb * 1440;
+                }
+                else if (typeBase == 2)
+                {
+                    resultat = nb * 60;
+                }
+                else
+                {
+                    resultat = nb;
+                }
+                Console.Write(Math.Truncate(resultat*100)/100);
             }
         }
     }
