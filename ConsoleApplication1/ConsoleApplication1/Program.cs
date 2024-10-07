c56a9b9e62ca6ab173f16e6b7823c602c9bcd3d1
Lucas <lucas.kominiarz@laposte.net>
2024-10-07 12:13:32 +0200

    caca

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace ConsoleApplication1
+﻿using System;
+
+namespace ConsoleApplication1
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            algo1 algo = new algo1(); 
+            int value = int.Parse(Console.ReadLine());
+            algo.Solve(value);
         }
     }
 }
--- ConsoleApplication1/ConsoleApplication1/algo1.cs
+++ ConsoleApplication1/ConsoleApplication1/algo1.cs
@@ -1,7 +1,29 @@
-﻿namespace ConsoleApplication1
+﻿using System;
+
+namespace ConsoleApplication1
 {
     public class algo1
     {
-        
+        public void Solve(int nb)
+        {
+            if (nb >= 6 && nb <= 874)
+            {
+                for (int i = 0; i < nb; i++)
+                {
+                    if (i % 3 == 0 || i % 4 == 0)
+                    {
+                        Console.Write("C");
+                    }
+                    if (i % 7 == 0 || i % 18 == 0)
+                    {
+                        Console.Write("A");
+                    }
+                    if (i % 42 == 0)
+                    {
+                        Console.Write("CA");
+                    }
+                }
+            }
+        }
     }
 }
